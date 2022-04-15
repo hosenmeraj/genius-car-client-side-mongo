485bd8e276cc47029150e32efa53665c354938d5
Merajhosen <merajakhon7@gmail.com>
2022-04-15 10:46:36 +0600

    RequireAuth login system and protect the checkout site

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetails = () => {
     const { serviceId } = useParams()
     return (
-        <div>
+        <div >
             <h1>This is service details page: {serviceId}</h1>
+            <div className='text-center '>
+                <Link to='/checkout'>
+                    <button className='btn btn-primary '>Procced Checkout</button>
+                </Link>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <footer className="text-center py-5 mt-3 bg-primary">
+        <footer className="text-center py-5 mt-3 bg-primary " >
             <p>CopyeWrite &copy; {year}</p>
         </footer>
     );
